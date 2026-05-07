7569522452eb218b796b5f0d4b5e711db6b371c2
shtfh <syj2003826@gmail.com>
2026-05-07 23:21:31 +0900

    setting: JWT 소셜 로그인 실습 프로젝트 초기 세팅

---
+++ src/test/java/com/skhu/jwtlogin/JwtloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhu.jwtlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
